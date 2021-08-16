8683087901bf6b34acccc386c0af4a037bd0f7dd
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-08-16 19:23:58 -0300

    start implement cookies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+// Solves config mismatch between Nest config and cookie-session
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['qwertyui0b'],
+    }),
+  );
   // strips validated object from unexpect properties, for security reasons
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
