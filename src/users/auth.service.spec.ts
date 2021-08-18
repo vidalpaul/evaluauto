deb50e18f556b04b6fc0e6fd10fad1d61597b6b2
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-08-18 10:38:26 -0300

    Update auth.service.spec.ts

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,13 +2,15 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
     // Create a fake copy of the users service
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User),
@@ -33,5 +35,15 @@ describe('AuthService', () => {
     const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
+  });
+
+  it('throws and error if user signs up with an email that is in use', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([
+        { id: 1, email: 'asdf@asdf.com', password: 'asdf' } as User,
+      ]);
+    await expect(
+      service.signup('asdf@asdf.com', 'pass'),
+    ).rejects.toBeInstanceOf(BadRequestException);
   });
 });
